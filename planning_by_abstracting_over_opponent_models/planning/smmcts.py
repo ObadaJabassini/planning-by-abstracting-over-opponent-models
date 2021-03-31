f2960625af73b2e60e1500d90481bdb45d5bfd67
Obada Aljabasini <obada.jabassini@gmail.com>
2021-03-31 13:09:50 +0200

    vectorize UCB

--- planning_by_abstracting_over_opponent_models/planning/smmcts.py
+++ planning_by_abstracting_over_opponent_models/planning/smmcts.py
@@ -1,53 +1,10 @@
-import math
 from copy import deepcopy
 
 import torch
 import torch.nn.functional as F
 
 from planning_by_abstracting_over_opponent_models.config import cpu
-
-
-class TreeNode:
-    def __init__(self,
-                 state,
-                 parent,
-                 is_terminal,
-                 value_estimate,
-                 action_prob_estimate,
-                 nb_players,
-                 action_space_size,
-                 exploration_coefs):
-        self.state = state
-        self.parent = parent
-        self.is_terminal = is_terminal
-        self.value_estimate = value_estimate
-        self.action_prob_estimate = action_prob_estimate
-        self.nb_players = nb_players
-        self.exploration_coefs = exploration_coefs
-        self.visit_count = 0
-        self.children = dict()
-        self.average_estimations = torch.zeros((nb_players, action_space_size))
-        self.nb_action_visits = torch.zeros((nb_players, action_space_size))
-
-    def select_best_actions(self):
-        return tuple(self.select_best_action_player(player) for player in range(self.nb_players))
-
-    def select_best_action_player(self, player):
-        uct = self.uct(player)
-        return uct.argmax().item()
-
-    def uct(self, player):
-        prob = self.action_prob_estimate[player]
-        exploration_coef = self.exploration_coefs[player]
-        x, n = self.average_estimations[player], self.nb_action_visits[player]
-        uct = x / n + exploration_coef * prob * torch.sqrt(math.log(self.visit_count) / n)
-        return uct
-
-    def update_actions_estimates(self, actions, action_value_estimate):
-        for i in range(len(actions)):
-            a = actions[i]
-            self.average_estimations[i][a] += action_value_estimate[i]
-            self.nb_action_visits[i][a] += 1
+from planning_by_abstracting_over_opponent_models.planning.tree_node import TreeNode
 
 
 class SMMCTS:
@@ -61,15 +18,16 @@ class SMMCTS:
         self.action_space_size = action_space_size
         self.extract_observation_func = extract_observation_func
         self.agent_model = agent_model
-        self.exploration_coefs = exploration_coefs
+        self.exploration_coefs = torch.as_tensor(exploration_coefs).view(nb_players, 1)
 
     def update(self, env, current_node: TreeNode):
         if current_node.is_terminal:
             return current_node.value_estimate
+        # select
         actions = current_node.select_best_actions()
         current_node.visit_count += 1
         state, rewards, is_terminal, _ = env.step(actions)
-
+        # expand
         if actions not in current_node.children:
             expected_value, action_probs = self.estimate_node(state)
             value_estimate = expected_value if not is_terminal else rewards
@@ -85,7 +43,8 @@ class SMMCTS:
 
         child = current_node.children[actions]
         value_estimate = self.update(env, child)
-        # current_node.value_estimate += value_estimate
+        # backpropagate
+        current_node.value_estimate += value_estimate
         current_node.update_actions_estimates(actions, value_estimate)
         return value_estimate
 
@@ -97,12 +56,24 @@ class SMMCTS:
         return value_estimate, action_probs_estimate
 
     def estimate_values(self, agent_value, opponent_values):
+        """
+        Estimate state's value using the agent model
+        :param agent_value:
+        :param opponent_values:
+        :return:
+        """
         agent_value = agent_value.view(-1).to(cpu)
         opponent_values = opponent_values.view(-1).to(cpu)
         value_estimate = torch.cat((agent_value, opponent_values))
         return value_estimate
 
     def estimate_action_probabilities(self, agent_action_log, opponent_action_log):
+        """
+        Estimate action probabilities using the agent model
+        :param agent_action_log:
+        :param opponent_action_log:
+        :return:
+        """
         agent_action_probs = F.softmax(agent_action_log, dim=-1)
         agent_action_probs = agent_action_probs.view((1, -1)).to(cpu)
         opponent_action_probs = F.softmax(opponent_action_log, dim=-1)
---
+++ planning_by_abstracting_over_opponent_models/planning/tree_node.py
@@ -0,0 +1,62 @@
+import math
+
+import torch
+
+
+class TreeNode:
+    def __init__(self,
+                 state,
+                 parent,
+                 is_terminal,
+                 value_estimate,
+                 action_prob_estimate,
+                 nb_players,
+                 action_space_size,
+                 exploration_coefs):
+        """
+
+        :param state: the associated state
+        :param parent: a pointer to the parent
+        :param is_terminal: if the state is terminal
+        :param value_estimate: the value estimate of the state, shape: (nb_players)
+        :param action_prob_estimate: the probabilities of each agent for each agent, shape: (nb_players, action_space_size)
+        :param nb_players: the number of players
+        :param action_space_size: the number of actions
+        :param exploration_coefs: the exploration coefficient for each agent
+        """
+        self.state = state
+        self.parent = parent
+        self.is_terminal = is_terminal
+        self.value_estimate = value_estimate
+        self.action_prob_estimate = action_prob_estimate
+        self.nb_players = nb_players
+        self.exploration_coefs = exploration_coefs
+        self.visit_count = 1
+        self.children = dict()
+        self.average_estimations = torch.zeros((nb_players, action_space_size))
+        self.nb_action_visits = torch.zeros((nb_players, action_space_size))
+
+    def select_best_actions(self):
+        uct = self.uct()
+        best_actions = torch.argmax(uct, dim=1)
+        return tuple(best_actions.tolist())
+
+    def uct(self):
+        probs = self.action_prob_estimate
+        c = self.exploration_coefs
+        x, n = self.average_estimations, self.nb_action_visits
+        x_bar = x / n
+        uct = x_bar + c * probs * torch.sqrt(math.log(self.visit_count) / n)
+        return uct
+
+    def update_actions_estimates(self, actions, action_value_estimate):
+        """
+
+        :param actions: the indicies of the action that should be updated, shape: (nb_players)
+        :param action_value_estimate: the estimated value function for each of those actions, shape: (nb_players)
+        :return:
+        """
+        for i in range(len(actions)):
+            a = actions[i]
+            self.average_estimations[i][a] += action_value_estimate[i]
+            self.nb_action_visits[i][a] += 1
