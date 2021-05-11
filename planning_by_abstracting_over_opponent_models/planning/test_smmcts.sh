45cc1b1953f2e5af94081cc50daa34f6b2e5e232
Obada Aljabasini <obada.jabassini@gmail.com>
2021-05-11 19:15:31 +0200

    Change script args

--- planning_by_abstracting_over_opponent_models/planning/test_smmcts.sh
+++ planning_by_abstracting_over_opponent_models/planning/test_smmcts.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-nohup python smmcts.py --nb-players 2 --nb-games 10 --nb-plays 10 --mcts-iterations 200 --use-python --fpu 100 &
+nohup python smmcts.py --multiprocessing --nb-players 2 --nb-games 10 --nb-plays 10 --mcts-iterations 200 --use-python --fpu 100 &
