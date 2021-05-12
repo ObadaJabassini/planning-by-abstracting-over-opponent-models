18cf1819b196e13a242772fbf5b3c55a47559f36
Obada Aljabasini <obada.jabassini@gmail.com>
2021-05-12 15:31:37 +0200

    Delete results.txt

--- planning_by_abstracting_over_opponent_models/planning/test_smmcts.sh
+++ planning_by_abstracting_over_opponent_models/planning/test_smmcts.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-nohup python -u smmcts.py --multiprocessing --nb-players 4 --nb-games 10 --nb-plays 10 --mcts-iterations 1500 --use-cython --fpu 1000 --hide-elapsed-time --no-progress-bar --use-simple-agent >> results.txt &
+nohup python -u smmcts.py --multiprocessing --nb-players 4 --nb-games 10 --nb-plays 10 --mcts-iterations 1500 --use-cython --fpu 1000 --hide-elapsed-time --no-progress-bar --use-simple-agent &
