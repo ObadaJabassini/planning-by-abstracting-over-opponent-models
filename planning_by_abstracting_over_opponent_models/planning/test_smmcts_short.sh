9caf3d0cd5ad379449029887c2c480f614666eba
Obada Aljabasini <obada.jabassini@gmail.com>
2021-05-19 14:24:05 +0200

    Add two option scripts

--- planning_by_abstracting_over_opponent_models/planning/test_smmcts_short.sh
+++ planning_by_abstracting_over_opponent_models/planning/test_smmcts_short.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python smmcts.py --multiprocessing --nb-players 4 --nb-games 10 --nb-plays 10 --mcts-iterations 1500 --fpu 1000 --hide-elapsed-time --no-progress-bar --use-simple-agent
+python smmcts.py --multiprocessing --nb-players 4 --nb-games 2 --nb-plays 5 --mcts-iterations 100 --fpu 1000 --hide-elapsed-time --no-progress-bar --use-simple-agent
