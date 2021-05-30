efa7dd396fc966655640a73e31ce2a1c9a3bf3ce
Obada Aljabasini <obada.jabassini@gmail.com>
2021-05-30 21:03:40 +0200

    Add tensorboard

--- planning_by_abstracting_over_opponent_models/learning/train_agent.sh
+++ planning_by_abstracting_over_opponent_models/learning/train_agent.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python -u learn.py --seed 32 --nb-steps 20 --save-interval 60 --nb-players 4 --opponent-class static --device cpu --monitoring --shared-opt
+python -u learn.py --seed 32 --nb-steps 20 --save-interval 60 --nb-players 4 --opponent-class static --device cpu --with-monitoring --shared-opt
