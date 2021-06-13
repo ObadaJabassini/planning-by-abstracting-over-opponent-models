727d13889cec9cf1322a6771f76fa6c1cdba7924
Obada Aljabasini <obada.jabassini@gmail.com>
2021-06-13 19:27:33 +0200

    Add new reward shaper

--- planning_by_abstracting_over_opponent_models/learning/train_agent_meteor.sh
+++ planning_by_abstracting_over_opponent_models/learning/train_agent_meteor.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python -u learn.py --opponent-class static --seed 32 --latent-dim 128 --head-dim 128 --hard-attention-rnn-hidden-size 128 --nb-steps 20 --save-interval 60
+python -u learn.py --opponent-class static --seed 32 --nb-steps 20 --save-interval 60
