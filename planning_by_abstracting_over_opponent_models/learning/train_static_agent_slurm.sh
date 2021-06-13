ad0fa3f1a692c00ca9d4d80a9e50839664a2bd99
Obada Aljabasini <obada.jabassini@gmail.com>
2021-06-13 19:05:38 +0200

    Add new reward shaper

--- planning_by_abstracting_over_opponent_models/learning/train_static_agent_slurm.sh
+++ planning_by_abstracting_over_opponent_models/learning/train_static_agent_slurm.sh
@@ -10,4 +10,4 @@
 
 export PYTHONPATH="$HOME/obada/planning-by-abstracting-over-opponent-models"
 cd "$HOME/obada/planning-by-abstracting-over-opponent-models/planning_by_abstracting_over_opponent_models/learning"
-python -u learn.py --nb-processes 24 --seed 32 --nb-steps 20 --save-interval 60 --opponent-class static
+python -u learn.py --nb-processes 24 --seed 32 --nb-steps 8 --save-interval 60 --opponent-class static
