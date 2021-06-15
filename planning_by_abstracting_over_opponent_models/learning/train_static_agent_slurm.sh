47614d2780dda480c7a41168404030d7b231a223
Obada Aljabasini <obada.jabassini@gmail.com>
2021-06-15 13:09:22 +0200

    Create new random agent

--- planning_by_abstracting_over_opponent_models/learning/train_static_agent_slurm.sh
+++ planning_by_abstracting_over_opponent_models/learning/train_static_agent_slurm.sh
@@ -10,4 +10,4 @@
 
 export PYTHONPATH="$HOME/obada/planning-by-abstracting-over-opponent-models"
 cd "$HOME/obada/planning-by-abstracting-over-opponent-models/planning_by_abstracting_over_opponent_models/learning"
-python -u learn.py --nb-processes 24 --save-interval 60 --opponent-classes "static, static, static"
+python -u learn.py --nb-processes 24 --save-interval 60 --max-grad-norm 40 --opponent-classes "static, static, static"
