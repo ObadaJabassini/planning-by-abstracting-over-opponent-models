38289d77534518fabf06035a32ec9f50d471ace3
Obada Aljabasini <obada.jabassini@gmail.com>
2021-06-18 12:19:27 +0200

    Visualizing value loss

--- planning_by_abstracting_over_opponent_models/learning/learn.py
+++ planning_by_abstracting_over_opponent_models/learning/learn.py
@@ -37,7 +37,7 @@ parser.add_argument('--nb-soft-attention-heads', type=int, default=4)
 parser.add_argument('--hard-attention-rnn-hidden-size', type=int, default=64)
 parser.add_argument('--approximate-hard-attention', dest='approximate_hard_attention', action='store_true')
 parser.add_argument('--exact-hard-attention', dest='approximate_hard_attention', action='store_false')
-parser.add_argument('--attention-operation', type=str, default="add")
+parser.add_argument('--attention-operation', type=str, default="multiply")
 parser.add_argument('--max-grad-norm', type=float, default=0.5)
 d = "ammo_usage, avoiding_flame, catching_enemy, consecutive_actions, enemy_killed, mobility, picking_powerup, planting_bomb, avoiding_illegal_moves"
 parser.add_argument('--reward-shapers',
--- planning_by_abstracting_over_opponent_models/learning/train_mixed_agent_slurm.sh
+++ planning_by_abstracting_over_opponent_models/learning/train_mixed_agent_slurm.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=24
 #SBATCH --error=slurm-mixed.err
 #SBATCH --output=slurm-mixed.out
-#SBATCH --time=48:05:00
+#SBATCH --time=60:05:00
 
 export PYTHONPATH="$HOME/obada/planning-by-abstracting-over-opponent-models"
 cd "$HOME/obada/planning-by-abstracting-over-opponent-models/planning_by_abstracting_over_opponent_models/learning"
