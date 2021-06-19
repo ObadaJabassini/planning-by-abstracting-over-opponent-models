67daa4d8bfb2eb3d118afa4d90bfa4d181db170e
Obada Aljabasini <obada.jabassini@gmail.com>
2021-06-19 21:23:10 +0200

    Simplify code

--- planning_by_abstracting_over_opponent_models/learning/model/opponent_model.py
+++ planning_by_abstracting_over_opponent_models/learning/model/opponent_model.py
@@ -5,13 +5,13 @@ class OpponentModel(nn.Module):
     def __init__(self, features_size, latent_dim, head_dim, nb_actions):
         super().__init__()
         self.latent_layer = nn.Sequential(nn.Linear(features_size, latent_dim), nn.ELU())
-        self.head_layer = nn.Sequential(nn.Linear(latent_dim, head_dim), nn.ELU())
+        # self.head_layer = nn.Sequential(nn.Linear(latent_dim, head_dim), nn.ELU())
         self.policy_layer = nn.Linear(head_dim, nb_actions)
         self.value_layer = nn.Linear(head_dim, 1)
 
     def forward(self, features):
         latent = self.latent_layer(features)
-        latent = self.head_layer(latent)
+        # latent = self.head_layer(latent)
         policy = self.policy_layer(latent)
         value = self.value_layer(latent)
         return latent, policy, value
