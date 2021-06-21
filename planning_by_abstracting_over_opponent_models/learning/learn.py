c8aced2231cc8ec47587534a6e8b8c35b610f975
Obada Aljabasini <obada.jabassini@gmail.com>
2021-06-21 12:21:57 +0200

    remove head layer

--- planning_by_abstracting_over_opponent_models/learning/learn.py
+++ planning_by_abstracting_over_opponent_models/learning/learn.py
@@ -87,7 +87,7 @@ if __name__ == '__main__':
     optimizer = None
     if args.shared_opt:
         optimizer = SharedAdam(shared_model.parameters(),
-                               lr=1e-5,
+                               lr=1e-4,
                                betas=(0.9, 0.999),
                                eps=1e-8,
                                weight_decay=1e-5)
