505d1ed44f3aa9549e1cc1d0b891fd516fb3fff1
Obada Aljabasini <obada.jabassini@gmail.com>
2021-06-22 11:12:34 +0200

    remove head layer

--- planning_by_abstracting_over_opponent_models/learning/monitor.py
+++ planning_by_abstracting_over_opponent_models/learning/monitor.py
@@ -35,30 +35,27 @@ def monitor(rank,
     actions = deque(maxlen=1000)
     episode_length = 0
     episodes = 0
-    try:
-        while True:
-            episode_length += 1
-            # Sync with the shared model
-            if done:
-                agent_model.load_state_dict(shared_model.state_dict())
-            with torch.no_grad():
-                obs = env.get_features(state).to(device)
-                agent_action = agent.act(obs, action_space)
-                opponents_action = env.act(state)
-                episode_actions = [agent_action, *opponents_action]
-                state, rewards, done = env.step(episode_actions)
+    while True:
+        episode_length += 1
+        # Sync with the shared model
+        if done:
+            agent_model.load_state_dict(shared_model.state_dict())
+        with torch.no_grad():
+            obs = env.get_features(state).to(device)
+            agent_action = agent.act(obs, action_space)
+            opponents_action = env.act(state)
+            episode_actions = [agent_action, *opponents_action]
+            state, rewards, done = env.step(episode_actions)
 
-            # a quick hack to prevent the agent from stucking
-            actions.append(agent_action)
-            if actions.count(actions[0]) == actions.maxlen:
-                done = True
-            if done:
-                episodes += 1
-                if episodes % save_interval == 0:
-                    torch.save(shared_model.state_dict(), f"../saved_models/{combined_opponent_classes}/agent_model_{episodes}.pt")
-                episode_length = 0
-                actions.clear()
-                state = env.reset()
-                time.sleep(60)
-    except:
-        torch.save(shared_model.state_dict(), f"../saved_models/{combined_opponent_classes}/agent_model.pt")
+        # a quick hack to prevent the agent from stucking
+        actions.append(agent_action)
+        if actions.count(actions[0]) == actions.maxlen:
+            done = True
+        if done:
+            episodes += 1
+            if episodes % save_interval == 0:
+                torch.save(shared_model.state_dict(), f"../saved_models/{combined_opponent_classes}/agent_model_{episodes}.pt")
+            episode_length = 0
+            actions.clear()
+            state = env.reset()
+            time.sleep(60)
