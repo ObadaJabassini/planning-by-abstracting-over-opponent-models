26e65485ec3ab53188177a9dbeaa208121621fd1
Obada Aljabasini <obada.jabassini@gmail.com>
2021-06-28 23:43:18 +0200

    Add reward

--- planning_by_abstracting_over_opponent_models/learning/learn.py
+++ planning_by_abstracting_over_opponent_models/learning/learn.py
@@ -36,7 +36,7 @@ parser.add_argument('--nb-soft-attention-heads', type=int, default=4)
 parser.add_argument('--hard-attention-rnn-hidden-size', type=int, default=None)
 parser.add_argument('--approximate-hard-attention', dest='approximate_hard_attention', action='store_true')
 parser.add_argument('--exact-hard-attention', dest='approximate_hard_attention', action='store_false')
-parser.add_argument('--max-grad-norm', type=float, default=0.5)
+parser.add_argument('--max-grad-norm', type=float, default=0.75)
 d = "enemy_killed, mobility, picking_powerup, planting_bomb_near_enemy, planting_bomb_near_wall, avoiding_illegal_moves"
 parser.add_argument('--reward-shapers',
                     type=lambda s: [str(item).strip().lower() for item in s.split(',')],
