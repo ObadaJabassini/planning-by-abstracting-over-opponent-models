aafeec870d607e3924a1cf4ce080e69d1b9e0eda
Obada Aljabasini <obada.jabassini@gmail.com>
2021-07-07 20:48:02 +0200

    ..

--- planning_by_abstracting_over_opponent_models/planning/test_smmcts_neural_network_slurm.sh
+++ planning_by_abstracting_over_opponent_models/planning/test_smmcts_neural_network_slurm.sh
@@ -7,7 +7,7 @@
 #SBATCH --error=slurm_smmcts_neural_network_%A_%a.err
 #SBATCH --output=slurm_smmcts_neural_network_%A_%a.out
 #SBATCH --time=96:05:00
-#SBATCH --array=1-10
+#SBATCH --array=1-5
 
 export PYTHONPATH="$HOME/obada/planning-by-abstracting-over-opponent-models"
 cd "$HOME/obada/planning-by-abstracting-over-opponent-models/planning_by_abstracting_over_opponent_models/planning"
