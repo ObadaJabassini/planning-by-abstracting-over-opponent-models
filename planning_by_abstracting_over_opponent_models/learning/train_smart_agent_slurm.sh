e43907fe9b7b768c76c03bd6446fb1750db2e8f0
Obada Aljabasini <obada.jabassini@gmail.com>
2021-07-09 12:30:20 +0200

    ..

--- planning_by_abstracting_over_opponent_models/learning/train_smart_agent_slurm.sh
+++ planning_by_abstracting_over_opponent_models/learning/train_smart_agent_slurm.sh
@@ -10,4 +10,4 @@
 
 export PYTHONPATH="$HOME/obada/planning-by-abstracting-over-opponent-models"
 cd "$HOME/obada/planning-by-abstracting-over-opponent-models/planning_by_abstracting_over_opponent_models/learning"
-python -u learn.py --nb-processes 49 --opponent-classes "smart_no_bomb, smart_no_bomb, smart_no_bomb"
+python -u learn.py --nb-processes 24 --opponent-classes "smart_no_bomb, smart_no_bomb, smart_no_bomb"
--- planning_by_abstracting_over_opponent_models/learning/train_static_agent_slurm.sh
+++ planning_by_abstracting_over_opponent_models/learning/train_static_agent_slurm.sh
@@ -10,4 +10,4 @@
 
 export PYTHONPATH="$HOME/obada/planning-by-abstracting-over-opponent-models"
 cd "$HOME/obada/planning-by-abstracting-over-opponent-models/planning_by_abstracting_over_opponent_models/learning"
-python -u learn.py --nb-processes 49 --opponent-classes "static, static, static"
+python -u learn.py --nb-processes 24 --opponent-classes "static, static, static"
